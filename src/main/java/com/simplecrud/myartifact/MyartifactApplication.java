bdce1bce4387f2ed48e625181223abf25d257483
comp <adenosiner@gmail.com>
2021-01-14 17:28:06 +0900

    first commit

---
+++ src/main/java/com/simplecrud/myartifact/MyartifactApplication.java
@@ -0,0 +1,13 @@
+package com.simplecrud.myartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyartifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplecrud/myartifact/MyartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplecrud.myartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
