b7c2827ff3dc4f1536880cc821025d4669e825f4
comp <adenosiner@gmail.com>
2021-01-14 18:11:27 +0900

    add sample Test TDD

---
+++ README.md
@@ -0,0 +1,9 @@
+# myartifact
+
+## simple CRUD app
+
+* Java 11
+* Spring
+* React.js
+* TypeScript
+* Sass
---
+++ src/test/java/com/simplecrud/myartifact/controller/ToDoService.java
@@ -0,0 +1,14 @@
+package com.simplecrud.myartifact.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToDoService {
+
+	public Object findAll() {
+		return new ArrayList<>();
+	}
+
+}
