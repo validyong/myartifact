32f85a403fa0574d88d58e8d0e1d8070b53d7d40
validyong <rokudynamics@gmail.com>
2021-02-15 18:26:20 +0900

    add user info

---
+++ src/main/java/com/simplecrud/myartifact/config/DataSourceConfig.java
@@ -0,0 +1,9 @@
+package com.simplecrud.myartifact.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+    
+    
+}
--- src/main/java/com/simplecrud/myartifact/repository/UserInfoRepository.java
+++ src/main/java/com/simplecrud/myartifact/repository/UserInfoRepository.java
@@ -5,6 +5,7 @@ import com.simplecrud.myartifact.mybean.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
+    UserInfo save(UserInfo userInfo);
     UserInfo findByUserId(String UserId);
     
 }
