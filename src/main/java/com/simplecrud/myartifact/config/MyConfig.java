ea3117d9891586a546957edcb333389fab60b2dc
validyong <rokudynamics@gmail.com>
2021-02-17 18:06:40 +0900

    remove springframework.boot:srping-boot-starter-security

--- src/main/java/com/simplecrud/myartifact/config/MyConfig.java
+++ src/main/java/com/simplecrud/myartifact/config/MyConfig.java
@@ -6,15 +6,11 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import oracle.ucp.jdbc.PoolDataSource;
 import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MyConfig {
     @Bean
     public DataSource dataSource() throws SQLException {
