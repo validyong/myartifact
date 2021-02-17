93132b003ad44c722d1b5636e4ce7c9e6a179ba4
validyong <rokudynamics@gmail.com>
2021-02-17 18:29:22 +0900

    set jwt filter

--- src/main/java/com/simplecrud/myartifact/config/MyConfig.java
+++ src/main/java/com/simplecrud/myartifact/config/MyConfig.java
@@ -4,10 +4,14 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import com.simplecrud.myartifact.filter.JwtAuthenticationFilter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import oracle.ucp.jdbc.PoolDataSource;
 import oracle.ucp.jdbc.PoolDataSourceFactory;
@@ -15,7 +19,7 @@ import oracle.ucp.jdbc.PoolDataSourceFactory;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MyConfig {
+public class MyConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public DataSource dataSource() throws SQLException {
         PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
@@ -27,6 +31,20 @@ public class MyConfig {
         dataSource.setMinPoolSize(5);
         dataSource.setMaxPoolSize(10);
         return dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and()
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/public").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+            .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
 }
---
+++ src/main/java/com/simplecrud/myartifact/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,7 @@
+package com.simplecrud.myartifact.filter;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    
+}
