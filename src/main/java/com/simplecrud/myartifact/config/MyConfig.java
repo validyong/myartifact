572d933b0b0270ac1aee59ce1be64f164fe225d1
validyong <rokudynamics@gmail.com>
2021-02-18 09:50:24 +0900

    delete

--- src/main/java/com/simplecrud/myartifact/config/MyConfig.java
+++ src/main/java/com/simplecrud/myartifact/config/MyConfig.java
@@ -4,8 +4,6 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import com.simplecrud.myartifact.filter.JwtAuthenticationFilter;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -31,20 +29,6 @@ public class MyConfig extends WebSecurityConfigurerAdapter {
         dataSource.setMinPoolSize(5);
         dataSource.setMaxPoolSize(10);
         return dataSource;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/api/public").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-            .addFilter(new JwtAuthorizationFilter(authenticationManager()))
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
 }
