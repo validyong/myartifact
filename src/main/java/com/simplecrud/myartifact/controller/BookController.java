7198284d4465ec1d1c64aab8b8655099b4fdb816
validyong <rokudynamics@gmail.com>
2021-02-19 14:59:58 +0900

    interrupt

--- src/main/java/com/simplecrud/myartifact/controller/BookController.java
+++ src/main/java/com/simplecrud/myartifact/controller/BookController.java
@@ -8,6 +8,7 @@ import com.simplecrud.myartifact.service.BookService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class BookController {
     @Autowired
